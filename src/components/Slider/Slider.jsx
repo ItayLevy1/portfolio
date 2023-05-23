0202bf682b7d51a3e9c2cb6cf5a1f56903513e71
ItayLevy1 <iti307@gmail.com>
2023-05-23 13:35:35 +0300

    fourth deploy commit

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -7,7 +7,23 @@ import 'Swiper/css/navigation';
 
 import '../../components/Slider/Slider.css';
 
+import art1 from '../../img/artwork/art1.jpeg';
+import art2 from '../../img/artwork/art2.jpeg';
+import art3 from '../../img/artwork/art3.jpeg';
+import art4 from '../../img/artwork/art4.jpeg';
+import art5 from '../../img/artwork/art5.jpeg';
+import art6 from '../../img/artwork/art6.jpeg';
+import art7 from '../../img/artwork/art7.jpeg';
+import art8 from '../../img/artwork/art8.jpeg';
+import art9 from '../../img/artwork/art9.jpeg';
+import art10 from '../../img/artwork/art10.jpeg';
+import art11 from '../../img/artwork/art11.jpeg';
+import art12 from '../../img/artwork/art12.jpeg';
+import art13 from '../../img/artwork/art13.jpeg';
+import art14 from '../../img/artwork/art14.jpeg';
+
 import { EffectCoverflow, Pagination, Navigation } from 'swiper';
+
 
 export default function Slider() {
     return (
@@ -37,46 +53,46 @@ export default function Slider() {
 
             >
                 <SwiperSlide>
-                    <img src="../../img/artwork/art1.jpeg" alt="slide_image" />
+                    <img src={art1} alt="slide_image" />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="../../img/artwork/art2.jpeg" alt="slide_image" />
+                    <img src={art2} alt="slide_image" />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="../../img/artwork/art3.jpeg" alt="slide_image" />
+                    <img src={art3} alt="slide_image" />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="../../img/artwork/art4.jpeg" alt="slide_image" />
+                    <img src={art4} alt="slide_image" />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="../../img/artwork/art5.jpeg" alt="slide_image" />
+                    <img src={art5} alt="slide_image" />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="../../img/artwork/art6.jpeg" alt="slide_image"/>
+                    <img src={art6} alt="slide_image"/>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="../../img/artwork/art7.jpeg" alt="slide_image"/>
+                    <img src={art7} alt="slide_image"/>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="../../img/artwork/art8.jpeg" alt="slide_image"/>
+                    <img src={art8} alt="slide_image"/>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="../../img/artwork/art9.jpeg" alt="slide_image"/>
+                    <img src={art9} alt="slide_image"/>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="../../img/artwork/art10.jpeg" alt="slide_image"/>
+                    <img src={art10} alt="slide_image"/>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="../../img/artwork/art11.jpeg" alt="slide_image"/>
+                    <img src={art11} alt="slide_image"/>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="../../img/artwork/art12.jpeg" alt="slide_image"/>
+                    <img src={art12} alt="slide_image"/>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="../../img/artwork/art13.jpeg" alt="slide_image"/>
+                    <img src={art13} alt="slide_image"/>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="../../img/artwork/art14.jpeg" alt="slide_image"/>
+                    <img src={art14} alt="slide_image"/>
                 </SwiperSlide>
 
                 <div className="slide-container">
